8693170ebc955b00fca6cfe060cb65adf680753a
FyuroTEX <yurilev4@gmail.com>
2018-06-02 18:02:51 +0300

    beauty

---
+++ js.js
@@ -0,0 +1,16 @@
+let searchUrl = 'https://wikipedia.org/w/api.php?action=opensearch&format=json&uselang=user&search=';
+let userInput;
+
+function sers0 () {
+  //noCanvas();
+
+userInput = document.getElementById('userinput').value;
+let srcSite =searchUrl + userInput;
+console.log(srcSite);
+
+
+  $.ajax({
+
+  });
+
+}
